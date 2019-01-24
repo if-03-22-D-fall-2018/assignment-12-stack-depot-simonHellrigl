a3c2ee8237d2dca68999bb18a872d4e090740c5e
simonHellrigl <simsi.hellrigl@gmail.com>
2019-01-24 13:12:22 +0100

    save

---
+++ depot.cpp
@@ -0,0 +1,50 @@
+
+#include "depot.h"
+
+struct DepotImplementation{
+    Stack stack_of_stacks;
+
+};
+
+Depot create_depot()
+{
+
+}
+
+/**
+ * Used to free all memory the supplied depot allocated.
+ * @param depot The depot to be deleted.
+ */
+void delete_depot(Depot depot)
+{
+
+}
+
+/**
+ * Adds a product to the depot.
+ * @param depot The depot on which to be pushed.
+ * @param product The product to be added.
+ */
+void push_depot(Depot depot, Product *product)
+{
+
+}
+
+/**
+ * @param depot The depot which count to be retrieved.
+ * @return The number of item stacks currently in the depot.
+ */
+int get_count(Depot depot)
+{
+
+}
+
+/**
+ * Removes and returns a product from the depot
+ * @param depot The depot from which the product to be taken.
+ * @return The removed product.
+ */
+Product* pop_depot(Depot depot)
+{
+
+}
---
+++ stack.cpp
@@ -0,0 +1,73 @@
+#include "stack.h"
+
+struct Node {
+    struct Node* next;
+    void* data;
+};
+
+struct StackImplementation {
+    struct Node* head;
+    int count;
+};
+
+/**
+ * Used to create a stack structure.
+ * @return The newly created stack.
+ */
+Stack create_stack()
+{
+    Stack stack = (Stack) smalloc(sizeof(StackImplementation));
+    stack->head = 0;
+    stack->count = 0;
+    return stack;
+
+}
+
+/**
+ * Used to free all memory the supplied stack allocated.
+ * @param stack The stack to be deleted.
+ * @note delete structure, including the struct itself, but not the data!
+ */
+void delete_stack(Stack stack)
+{
+
+}
+
+/**
+ * Adds a new node with the supplied data to the top of the stack.
+ * @param stack The stack onto which data has to be pushed.
+ * @param data The data to be pushed.
+ */
+void push_stack(Stack stack, void *data)
+{
+
+}
+
+/**
+ * @param stack The stack which number of elements to be retrieved.
+ * @return The number of items currently on the stack.
+ */
+int get_count(Stack stack)
+{
+
+}
+
+/**
+ * Removes and returns the topmost item of the stack.
+ * @param stack The stack from which to take.
+ * @return The element returned from the stack.
+ */
+void* pop_stack(Stack stack)
+{
+
+}
+
+/**
+ * Returns the topmost item of the stack without removing it.
+ * @param stack The stack from which to take.
+ * @return The element returned from the stack.
+*/
+void* peek_stack(Stack stack)
+{
+
+}
